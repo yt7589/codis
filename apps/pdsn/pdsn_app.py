ee51833d6f85e9c77d5424dd617b9c4b0e14d20b
yt7589 <yt7589@qq.com>
2020-05-26 10:33:25 +0800

    learn FeaturePyramidNetwork

--- apps/pdsn/pdsn_app.py
+++ apps/pdsn/pdsn_app.py
@@ -17,7 +17,7 @@ class PdsnApp(object):
         i_debug = 1
         if 1 == i_debug:
             exp = PdsnExp()
-            exp.exp_intermediate_layer_getter()
+            exp.exp_feature_pyramid_network()
             return
         device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
         # our dataset has two classes only - background and person
--- apps/pdsn/pdsn_exp.py
+++ apps/pdsn/pdsn_exp.py
@@ -7,10 +7,21 @@ class PdsnExp(object):
         self.name = ''
 
     def exp_intermediate_layer_getter(self):
-        m = torchvision.models.resnet18(pretrained=True)
+        m = torchvision.models.resnet50(pretrained=True)
         new_m = torchvision.models._utils.IntermediateLayerGetter(
             m, {'layer1': 'feat1', 'layer2': 'feat2', 'layer3': 'feat3', 'layer4': 'feat4'}
         )
         out = new_m(torch.rand(1, 3, 224, 224))
         print([(k, v.shape) for k, v in out.items()])
+
+    def exp_feature_pyramid_network(self):
+        fpn = torchvision.ops.FeaturePyramidNetwork([10, 20, 30], 5)
+        m = torchvision.models.resnet50(pretrained=True)
+        new_m = torchvision.models._utils.IntermediateLayerGetter(
+            m, {'layer1': 'feat1', 'layer2': 'feat2', 'layer3': 'feat3', 'layer4': 'feat4'}
+        )
+        x = new_m(torch.rand(1, 3, 224, 224))
+        out = fpn(x)
+        for k, v in out.items():
+            print('{0}:{1};'.format(k, v.shape))
         
