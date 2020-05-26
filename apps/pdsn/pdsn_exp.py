55ada06efe9d7ac11ef917fd34ee6f4c217ed34a
yt7589 <yt7589@qq.com>
2020-05-26 10:40:04 +0800

    run the official example of FeaturePyramidNetwork

--- apps/pdsn/pdsn_exp.py
+++ apps/pdsn/pdsn_exp.py
@@ -15,6 +15,7 @@ class PdsnExp(object):
         print([(k, v.shape) for k, v in out.items()])
 
     def exp_feature_pyramid_network(self):
+        '''
         fpn = torchvision.ops.FeaturePyramidNetwork([64, 128, 256, 512], 32)
         m = torchvision.models.resnet50(pretrained=True)
         new_m = torchvision.models._utils.IntermediateLayerGetter(
@@ -24,4 +25,12 @@ class PdsnExp(object):
         out = fpn(x)
         for k, v in out.items():
             print('{0}:{1};'.format(k, v.shape))
+        '''
+        m = torchvision.ops.FeaturePyramidNetwork([10, 20, 30], 5)
+        x = OrderedDict()
+        x['feat0'] = torch.rand(1, 10, 64, 64)
+        x['feat2'] = torch.rand(1, 20, 16, 16)
+        x['feat3'] = torch.rand(1, 30, 8, 8)
+        output = m(x)
+        print([(k, v.shape) for k, v in output.items()])
         
