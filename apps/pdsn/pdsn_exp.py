ceb248925295c2c291e68a5e607a5724abf0f6fa
yt7589 <yt7589@qq.com>
2020-05-26 10:06:57 +0800

    get all the block of resnet50

--- apps/pdsn/pdsn_exp.py
+++ apps/pdsn/pdsn_exp.py
@@ -9,7 +9,7 @@ class PdsnExp(object):
     def exp_intermediate_layer_getter(self):
         m = torchvision.models.resnet18(pretrained=True)
         new_m = torchvision.models._utils.IntermediateLayerGetter(
-            m, {'layer1': 'feat1', 'layer3': 'feat2'}
+            m, {'layer1': 'feat1', 'layer2': 'feat2', 'layer3': 'feat3', 'layer4': 'feat4'}
         )
         out = new_m(torch.rand(1, 3, 224, 224))
         print([(k, v.shape) for k, v in out.items()])
