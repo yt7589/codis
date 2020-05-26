9a21ff5f38ad5a7efb7b2d47aca169a416a78d54
yt7589 <yt7589@qq.com>
2020-05-26 11:52:07 +0800

    switch to resnet50 backbone

--- apps/pdsn/pdsn_exp.py
+++ apps/pdsn/pdsn_exp.py
@@ -17,12 +17,12 @@ class PdsnExp(object):
 
     def exp_feature_pyramid_network(self):
         '''
-        fpn = torchvision.ops.FeaturePyramidNetwork([64, 128, 256, 512], 32)
         out = fpn(x)
         for k, v in out.items():
             print('{0}:{1};'.format(k, v.shape))
         '''
-        fpn = torchvision.ops.FeaturePyramidNetwork([10, 20, 30], 5)
+        fpn = torchvision.ops.FeaturePyramidNetwork([256, 512, 1024, 2048], 32)
+        # fpn = torchvision.ops.FeaturePyramidNetwork([10, 20, 30], 5)
         m = torchvision.models.resnet50(pretrained=True)
         new_m = torchvision.models._utils.IntermediateLayerGetter(
             m, {'layer1': 'feat1', 'layer2': 'feat2', 'layer3': 'feat3', 'layer4': 'feat4'}
@@ -30,10 +30,12 @@ class PdsnExp(object):
         x0 = new_m(torch.rand(1, 3, 224, 224))
         for k, v in x0.items():
             print('{0}:{1};'.format(k, v.shape))
+        '''
         x = OrderedDict()
         x['feat0'] = torch.rand(1, 10, 64, 64)
         x['feat2'] = torch.rand(1, 20, 16, 16)
         x['feat3'] = torch.rand(1, 30, 8, 8)
-        output = fpn(x)
+        '''
+        output = fpn(x0)
         print([(k, v.shape) for k, v in output.items()])
         
