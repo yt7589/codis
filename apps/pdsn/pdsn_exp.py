a6afe601b55f62feab9cd49ed9d924969f2cc629
yt7589 <yt7589@qq.com>
2020-05-26 10:44:59 +0800

    compare resnet50 intermediate layer with custome data

--- apps/pdsn/pdsn_exp.py
+++ apps/pdsn/pdsn_exp.py
@@ -18,20 +18,22 @@ class PdsnExp(object):
     def exp_feature_pyramid_network(self):
         '''
         fpn = torchvision.ops.FeaturePyramidNetwork([64, 128, 256, 512], 32)
-        m = torchvision.models.resnet50(pretrained=True)
-        new_m = torchvision.models._utils.IntermediateLayerGetter(
-            m, {'layer1': 'feat1', 'layer2': 'feat2', 'layer3': 'feat3', 'layer4': 'feat4'}
-        )
-        x = new_m(torch.rand(1, 3, 224, 224))
         out = fpn(x)
         for k, v in out.items():
             print('{0}:{1};'.format(k, v.shape))
         '''
-        m = torchvision.ops.FeaturePyramidNetwork([10, 20, 30], 5)
+        fpn = torchvision.ops.FeaturePyramidNetwork([10, 20, 30], 5)
+        m = torchvision.models.resnet50(pretrained=True)
+        new_m = torchvision.models._utils.IntermediateLayerGetter(
+            m, {'layer1': 'feat1', 'layer2': 'feat2', 'layer3': 'feat3', 'layer4': 'feat4'}
+        )
+        x0 = new_m(torch.rand(1, 3, 224, 224))
+        for k, v in x0.items():
+            print('{0}:{1};'.format(k, v.shape))
         x = OrderedDict()
         x['feat0'] = torch.rand(1, 10, 64, 64)
         x['feat2'] = torch.rand(1, 20, 16, 16)
         x['feat3'] = torch.rand(1, 30, 8, 8)
-        output = m(x)
+        output = fpn(x)
         print([(k, v.shape) for k, v in output.items()])
         
