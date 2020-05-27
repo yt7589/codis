36b89bf11997e9dcc4c4038d432c40c0575dc367
yt7589 <yt7589@qq.com>
2020-05-27 21:55:02 +0800

    fix BUG in import load_state_dict_from_url

--- ann/resnet.py
+++ ann/resnet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
-from torchvision.utils import load_state_dict_from_url
+#from torchvision.utils import load_state_dict_from_url
+from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
