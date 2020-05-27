f9fce5d5d0583db216f384a9a958445cd1b6c4ee
yt7589 <yt7589@qq.com>
2020-05-27 17:23:04 +0800

    learn MaskRCNN architecture

--- apps/pdsn/pdsn_exp.py
+++ apps/pdsn/pdsn_exp.py
@@ -2,6 +2,9 @@
 from collections import OrderedDict
 import torch
 import torchvision
+from ann.mask_rcnn import MaskRCNN
+from ann.rpn import AnchorGenerator
+import ann.resnet as resnet
 
 class PdsnExp(object):
     def __init__(self):
@@ -16,13 +19,6 @@ class PdsnExp(object):
         print([(k, v.shape) for k, v in out.items()])
 
     def exp_feature_pyramid_network(self):
-        '''
-        out = fpn(x)
-        for k, v in out.items():
-            print('{0}:{1};'.format(k, v.shape))
-        '''
-        fpn = torchvision.ops.FeaturePyramidNetwork([256, 512, 1024, 2048], 32)
-        # fpn = torchvision.ops.FeaturePyramidNetwork([10, 20, 30], 5)
         m = torchvision.models.resnet50(pretrained=True)
         new_m = torchvision.models._utils.IntermediateLayerGetter(
             m, {'layer1': 'feat1', 'layer2': 'feat2', 'layer3': 'feat3', 'layer4': 'feat4'}
@@ -31,8 +27,13 @@ class PdsnExp(object):
         print('Features:')
         for k, v in x0.items():
             print('{0}:{1};'.format(k, v.shape))
+        fpn = torchvision.ops.FeaturePyramidNetwork([256, 512, 1024, 2048], 32)
         output = fpn(x0)
         print('FPNs:')
         for k, v in output.items():
             print('{0}:{1};'.format(k, v.shape))
+
+    def exp_mask_rcnn(self):
+        backbone = resnet.resnet50(pretrained=True)
+
         
