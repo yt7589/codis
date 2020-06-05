59a1ee065a537f5acea7bf01a6c892eae40540b7
yt7589 <yt7589@qq.com>
2020-06-05 23:25:11 +0800

    fix BUG20200605006

--- apps/ecv/ecv_app.py
+++ apps/ecv/ecv_app.py
@@ -10,5 +10,6 @@ class EcvApp(object):
         print('测试平台')
         dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
         model = EcvModel(dev)
+        x = torch.rand(1, 3, 224, 224)
         y_hat = model(x)
         print('y_hat: {0};'.format(y_hat))
