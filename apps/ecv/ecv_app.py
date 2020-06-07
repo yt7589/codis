0b480b1d93235593503d34fe7c76b6188479f328
yt7589 <yt7589@qq.com>
2020-06-07 14:25:21 +0800

    add dataset implementation

--- apps/ecv/ecv_app.py
+++ apps/ecv/ecv_app.py
@@ -1,6 +1,57 @@
 #
 import torch
 from apps.ecv.ecv_model import EcvModel
+from apps.ecv.ecv_config import LoadConfig, load_data_transformers
+from apps.ecv.ecv_dataset import collate_fn4train, collate_fn4val, collate_fn4test, collate_fn4backbone, EcvDataset
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='dcl parameters')
+    parser.add_argument('--data', dest='dataset',
+                        default='CUB', type=str)
+    parser.add_argument('--save', dest='resume',
+                        default=None,
+                        type=str)
+    parser.add_argument('--backbone', dest='backbone',
+                        default='resnet50', type=str)
+    parser.add_argument('--auto_resume', dest='auto_resume',
+                        action='store_true')
+    parser.add_argument('--epoch', dest='epoch',
+                        default=360, type=int)
+    parser.add_argument('--tb', dest='train_batch',
+                        default=8, type=int)
+    parser.add_argument('--vb', dest='val_batch',
+                        default=512, type=int)
+    parser.add_argument('--sp', dest='save_point',
+                        default=5000, type=int)
+    parser.add_argument('--cp', dest='check_point',
+                        default=5000, type=int)
+    parser.add_argument('--lr', dest='base_lr',
+                        default=0.0008, type=float)
+    parser.add_argument('--lr_step', dest='decay_step',
+                        default=60, type=int)
+    parser.add_argument('--cls_lr_ratio', dest='cls_lr_ratio',
+                        default=10.0, type=float)
+    parser.add_argument('--start_epoch', dest='start_epoch',
+                        default=0,  type=int)
+    parser.add_argument('--tnw', dest='train_num_workers',
+                        default=16, type=int)
+    parser.add_argument('--vnw', dest='val_num_workers',
+                        default=32, type=int)
+    parser.add_argument('--detail', dest='discribe',
+                        default='', type=str)
+    parser.add_argument('--size', dest='resize_resolution',
+                        default=512, type=int)
+    parser.add_argument('--crop', dest='crop_resolution',
+                        default=448, type=int)
+    parser.add_argument('--cls_2', dest='cls_2',
+                        action='store_true')
+    parser.add_argument('--cls_mul', dest='cls_mul',
+                        action='store_true')
+    parser.add_argument('--swap_num', default=[7, 7],
+                    nargs=2, metavar=('swap1', 'swap2'),
+                    type=int, help='specify a range')
+    args = parser.parse_args()
+    return args
 
 class EcvApp(object):
     def __init__(self):
@@ -13,3 +64,67 @@ class EcvApp(object):
         x = torch.rand(1, 3, 224, 224)
         y_hat = model(x)
         print('y_hat: {0};'.format(y_hat.shape))
+        # 测试数据集的使用
+        args = parse_args()
+        config = LoadConfig(args, 'train')
+        config.cls_2 = args.cls_2
+        config.cls_2xmul = args.cls_mul
+        transformers = load_data_transformers(args.resize_resolution, args.crop_resolution, args.swap_num)
+        # inital dataloader
+        train_set = dataset(Config = config,\
+                            anno = config.train_anno,\
+                            common_aug = transformers["common_aug"],\
+                            swap = transformers["swap"],\
+                            swap_size=args.swap_num, \
+                            totensor = transformers["train_totensor"],\
+                            train = True)
+
+        trainval_set = dataset(config = config,\
+                            anno = config.train_anno,\
+                            common_aug = transformers["None"],\
+                            swap = transformers["None"],\
+                            swap_size=args.swap_num, \
+                            totensor = transformers["val_totensor"],\
+                            train = False,
+                            train_val = True)
+
+        val_set = dataset(config = config,\
+                        anno = config.val_anno,\
+                        common_aug = transformers["None"],\
+                        swap = transformers["None"],\
+                            swap_size=args.swap_num, \
+                        totensor = transformers["test_totensor"],\
+                        test=True)
+
+        dataloader = {}
+        dataloader['train'] = torch.utils.data.DataLoader(train_set,\
+                                                    batch_size=args.train_batch,\
+                                                    shuffle=True,\
+                                                    num_workers=args.train_num_workers,\
+                                                    collate_fn=collate_fn4train if not config.use_backbone else collate_fn4backbone,
+                                                    drop_last=True if config.use_backbone else False,
+                                                    pin_memory=True)
+
+        setattr(dataloader['train'], 'total_item_len', len(train_set))
+
+        dataloader['trainval'] = torch.utils.data.DataLoader(trainval_set,\
+                                                    batch_size=args.val_batch,\
+                                                    shuffle=False,\
+                                                    num_workers=args.val_num_workers,\
+                                                    collate_fn=collate_fn4val if not config.use_backbone else collate_fn4backbone,
+                                                    drop_last=True if config.use_backbone else False,
+                                                    pin_memory=True)
+
+        setattr(dataloader['trainval'], 'total_item_len', len(trainval_set))
+        setattr(dataloader['trainval'], 'num_cls', config.numcls)
+
+        dataloader['val'] = torch.utils.data.DataLoader(val_set,\
+                                                    batch_size=args.val_batch,\
+                                                    shuffle=False,\
+                                                    num_workers=args.val_num_workers,\
+                                                    collate_fn=collate_fn4test if not config.use_backbone else collate_fn4backbone,
+                                                    drop_last=True if config.use_backbone else False,
+                                                    pin_memory=True)
+
+        setattr(dataloader['val'], 'total_item_len', len(val_set))
+        setattr(dataloader['val'], 'num_cls', config.numcls)
---
+++ apps/ecv/ecv_dataset.py
@@ -0,0 +1,228 @@
+# coding=utf8
+from __future__ import division
+import sys
+import os
+import torch
+import torch.utils.data as data
+import pandas as pd
+import random
+import PIL.Image as Image
+from PIL import ImageStat
+
+import pdb
+
+def random_sample(img_names, labels):
+    anno_dict = {}
+    img_list = []
+    anno_list = []
+    for img, anno in zip(img_names, labels):
+        if not anno in anno_dict:
+            anno_dict[anno] = [img]
+        else:
+            anno_dict[anno].append(img)
+
+    for anno in anno_dict.keys():
+        anno_len = len(anno_dict[anno])
+        fetch_keys = random.sample(list(range(anno_len)), anno_len//10)
+        img_list.extend([anno_dict[anno][x] for x in fetch_keys])
+        anno_list.extend([anno for x in fetch_keys])
+    return img_list, anno_list
+
+
+
+class EcvDataset(data.Dataset):
+    def __init__(self, Config, anno, swap_size=[7,7], common_aug=None, 
+                swap=None, totensor=None, train=False, 
+                train_val=False, test=False):
+        self.root_path = Config.rawdata_root
+        self.numcls = Config.numcls
+        self.dataset = Config.dataset
+        self.use_cls_2 = Config.cls_2
+        self.use_cls_mul = Config.cls_2xmul
+        if isinstance(anno, pd.core.frame.DataFrame):
+            self.paths = anno['ImageName'].tolist()
+            self.labels = anno['label'].tolist()
+            shuffle_samples(self.paths, self.labels)
+            #self.labels = list(map(int, self.labels))
+        elif isinstance(anno, dict):
+            self.paths = anno['img_name']
+            self.labels = anno['label']
+            shuffle_samples(self.paths, self.labels)
+            #self.labels = list(map(int, self.labels))
+
+        if train_val:
+            #self.paths, self.labels = random_sample(self.paths, self.labels)
+            self.paths = self.paths[:300]
+            self.labels = self.labels[:300]
+        self.common_aug = common_aug
+        self.swap = swap
+        self.totensor = totensor
+        self.cfg = Config
+        self.train = train
+        self.swap_size = swap_size
+        self.test = test
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, item):
+        #img_path = os.path.join(self.root_path, self.paths[item])
+        img_path = self.paths[item]
+        img = self.pil_loader(img_path)
+        if self.test:
+            img = self.totensor(img)
+            label = self.labels[item]
+            return img, label, self.paths[item]
+        img_unswap = self.common_aug(img) if not self.common_aug is None else img
+        image_unswap_list = self.crop_image(img_unswap, self.swap_size)
+
+        swap_range = self.swap_size[0] * self.swap_size[1]
+        swap_law1 = [(i-(swap_range//2))/swap_range for i in range(swap_range)]
+
+        if self.train:
+            img_swap = self.swap(img_unswap)
+            image_swap_list = self.crop_image(img_swap, self.swap_size)
+            unswap_stats = [sum(ImageStat.Stat(im).mean) for im in image_unswap_list]
+            swap_stats = [sum(ImageStat.Stat(im).mean) for im in image_swap_list]
+            swap_law2 = []
+            for swap_im in swap_stats:
+                distance = [abs(swap_im - unswap_im) for unswap_im in unswap_stats]
+                index = distance.index(min(distance))
+                swap_law2.append((index-(swap_range//2))/swap_range)
+            img_swap = self.totensor(img_swap)
+            label = self.labels[item]
+            if self.use_cls_mul:
+                #label_swap = label + self.numcls
+                #if not isinstance(label, int):
+                #    print(type(label))
+                label_swap = int(label) + self.numcls
+            if self.use_cls_2:
+                label_swap = -1
+            img_unswap = self.totensor(img_unswap)
+            return img_unswap, img_swap, label, label_swap, swap_law1, swap_law2, self.paths[item]
+        else:
+            label = self.labels[item]
+            swap_law2 = [(i-(swap_range//2))/swap_range for i in range(swap_range)]
+            label_swap = label
+            img_unswap = self.totensor(img_unswap)
+            return img_unswap, label, label_swap, swap_law1, swap_law2, self.paths[item]
+
+    def pil_loader(self,imgpath):
+        with open(imgpath, 'rb') as f:
+            with Image.open(f) as img:
+                return img.convert('RGB')
+
+    def crop_image(self, image, cropnum):
+        width, high = image.size
+        crop_x = [int((width / cropnum[0]) * i) for i in range(cropnum[0] + 1)]
+        crop_y = [int((high / cropnum[1]) * i) for i in range(cropnum[1] + 1)]
+        im_list = []
+        for j in range(len(crop_y) - 1):
+            for i in range(len(crop_x) - 1):
+                im_list.append(image.crop((crop_x[i], crop_y[j], min(crop_x[i + 1], width), min(crop_y[j + 1], high))))
+        return im_list
+
+
+    def get_weighted_sampler(self):
+        img_nums = len(self.labels)
+        weights = [self.labels.count(x) for x in range(self.numcls)]
+        return torch.utils.data.sampler.WeightedRandomSampler(weights, num_samples=img_nums)
+
+
+def collate_fn4train(batch):
+    imgs = []
+    label = []
+    label_swap = []
+    law_swap = []
+    img_name = []
+    #img_unswap, img_swap, label, label_swap, swap_law1, swap_law2, self.paths[item]
+    for sample in batch:
+        imgs.append(sample[0])
+        imgs.append(sample[1])
+        label.append(sample[2])
+        label.append(sample[2])
+        if sample[3] == -1:
+            label_swap.append(1)
+            label_swap.append(0)
+        else:
+            label_swap.append(sample[2])
+            label_swap.append(sample[3])
+        law_swap.append(sample[4])
+        law_swap.append(sample[5])
+        img_name.append(sample[-1])
+    return torch.stack(imgs, 0), label, label_swap, law_swap, img_name
+
+def collate_fn4val(batch):
+    imgs = []
+    label = []
+    label_swap = []
+    law_swap = []
+    img_name = []
+    for sample in batch:
+        imgs.append(sample[0])
+        label.append(sample[1])
+        if sample[3] == -1:
+            label_swap.append(1)
+        else:
+            label_swap.append(sample[2])
+        law_swap.append(sample[3])
+        img_name.append(sample[-1])
+    return torch.stack(imgs, 0), label, label_swap, law_swap, img_name
+
+def collate_fn4backbone(batch):
+    imgs = []
+    label = []
+    img_name = []
+    for sample in batch:
+        imgs.append(sample[0])
+        if len(sample) == 7:
+            label.append(sample[2])
+        else:
+            label.append(sample[1])
+        img_name.append(sample[-1])
+    return torch.stack(imgs, 0), label, img_name
+
+
+def collate_fn4test(batch):
+    imgs = []
+    label = []
+    img_name = []
+    for sample in batch:
+        imgs.append(sample[0])
+        label.append(sample[1])
+        img_name.append(sample[-1])
+    return torch.stack(imgs, 0), label, img_name
+
+def preprocess_anno():
+    '''
+    将训练样本集中的数据，从文件在硬盘中的顺序变为随机顺序，
+    保证训练过程中的随机性
+    '''
+    anno_file = './datasets/CUB_200_2011/anno/ct_train_newd.txt'
+    print(anno_file)
+    anno = pd.read_csv(anno_file, sep="*", header=None, 
+                names=['ImageName', 'label'])
+    if isinstance(anno, pd.core.frame.DataFrame):
+        paths = anno['ImageName'].tolist()
+        labels = anno['label'].tolist()
+        #self.labels = list(map(int, self.labels))
+    elif isinstance(anno, dict):
+        paths = anno['img_name']
+        labels = anno['label']
+    shuffle_samples(paths, labels)
+    paths_len = len(paths)
+    for i in range(0, paths_len):
+        print('{0} => {1};'.format(paths[i], labels[i]))
+
+def shuffle_samples(paths, labels):
+    paths_len = len(paths)
+    labels_len = len(labels)
+    print('{0}={1}?'.format(paths_len, labels_len))
+    for i in range(1, paths_len):
+        rn = random.randint(0, paths_len-1)
+        temp_path = paths[rn]
+        paths[rn] = paths[i]
+        paths[i] = temp_path
+        temp_label = labels[rn]
+        labels[rn] = labels[i]
+        labels[i] = temp_label
